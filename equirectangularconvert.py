6aa1b74b0088c1cb1086866ff85851d8510a0469
Michael G. Inso <68110223+MiChaelinzo@users.noreply.github.com>
2023-03-12 19:24:14 +0300

    Create

---
+++ equirectangularconvert.py
@@ -0,0 +1,32 @@
+from PIL import Image
+
+def convert_to_equirectangular(image_path, width, height):
+    # Open the image
+    im = Image.open(image_path)
+    
+    # Resize the image to the desired width and height
+    im = im.resize((width, height))
+    
+    # Create a new image with the same width and height, and white background
+    eqr_image = Image.new('RGB', (width, height), (255, 255, 255))
+    
+    # Map the pixels from the original image onto the equirectangular image
+    for x in range(width):
+        for y in range(height):
+            lon = (x / width) * 360 - 180
+            lat = (y / height) * 180 - 90
+            pixel = im.getpixel((x, y))
+            eqr_x = int(((lon + 180) / 360) * width)
+            eqr_y = int(((lat + 90) / 180) * height)
+            eqr_image.putpixel((eqr_x, eqr_y), pixel)
+    
+    return eqr_image
+
+image_path = 'C:\\Users\\micha\\Downloads\\cyberpunk-2077-grimes-1.png'
+width = 4096
+height = 2048
+
+eqr_image = convert_to_equirectangular(image_path, width, height)
+
+# Save the equirectangular image
+eqr_image.save('C:\\Users\\micha\\Documents\\equirectangular.png')
